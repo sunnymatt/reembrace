56e222d76a4067593ebcf25086f86793d70d5269
Matthew Sun <matthewkol186@gmail.com>
2019-02-23 00:28:05 -0800

    Added more information to the housing section

--- models/init.js
+++ models/init.js
@@ -59,6 +59,56 @@ function populateInfo() {
         });
         housing.save(function(err, obj) {
             if (err) throw err;
+            var vash = new SurveyQuestion({
+                question: "The HUD-VASH program provides assistance to homeless veterans by combining rental assistance with case management and clinical services. The HUD-VASH program is a partnership between the Veterans Affairs Palo Alto Health Care System (VA Palo Alto) and SCCHA.\n\n To apply, you need a referral from VA Palo Alto. You can schedule an appointment online at https://www.myhealth.va.gov/mhv-portal-web/web/myhealthevet/keeping-up-with-all-your-va-appointments. You can also call them at 800-455-0057 or (650) 493-5000.",
+                option: 'VASH',
+                apply: true,
+                parent: obj._id,
+            });
+            vash.save(function(err, obj) {
+                if (err) throw err;
+            })
+            var eden = new SurveyQuestion({
+                question: "The mission of Eden Housing is to build and maintain high quality, well-managed, service-enhanced affordable housing communities that meet the diverse needs of lower income families, seniors, and persons with disabilities.\n\nYou can view a property locator online at https://www.edenhousing.org/interactive-map?field_county__value=11&field_field_address_locality=34. You may also call Eden Housing at (510) 582-1460.",
+                option: 'Eden Housing',
+                parent: obj._id,
+            });
+            eden.save(function(err, obj) {
+                if (err) throw err;
+            })
+            var mercy = new SurveyQuestion({
+                question: "Mercy Housing provides a wide range of affordable, low-income apartment rental opportunities across the United States. Our housing services are available for families, seniors and people with special needs, including those with developmental disabilities, HIV/AIDS, formerly homeless individuals and Veterans.\n\nYou can view property listings for the San Francisco Bay Area at https://www.mercyhousing.org/california-properties#San%20Fran%20Properties.\n\nIf you have general questions about low-income assistance or anything else, please contact the corporate office at 866-338-0557.",
+                option: 'Eden Housing',
+                parent: obj._id,
+            });
+            mercy.save(function(err, obj) {
+                if (err) throw err;
+            })
+            var mercy = new SurveyQuestion({
+                question: "First Community Housing is an affordable housing developer. To find housing now, you can visit https://www.firstcommunityhousing.org/findhousing. For a listing of all affordable housing available in Santa Clara County, go to http://www.scchousingsearch.org. You can also call toll-free at 1-877-428-8844 Monday-Friday from 12pm-11pm.",
+                option: 'First Community Housing',
+                parent: obj._id,
+            });
+            mercy.save(function(err, obj) {
+                if (err) throw err;
+            })
+
+            var homefirst = new SurveyQuestion({
+                question: "HomeFirst is a leading provider of services, shelter, and housing opportunities to the homeless and those at risk of homelessness in Santa Clara County. HomeFirst's Boccardo Reception Center is the largest homeless service center in Santa Clara County. It operates year-round, 24 hours a day, providing shelter and transitional housing as well as a wide array of critical services. The BRC is located at 2011 Little Orchard St, San Jose, CA 95125.\n\nHomeFirst also has a Cold Weather Shelter Program. HomeFirst’s CWSP provides emergency shelter, showers, meals, medical services, and a safe environment for homeless individuals in North and South County.\n\nTo reserve a space in South County Gilroy Shelter, contact Kenneth Rideout: 408-489-8781, KRideout@homefirstscc.org.\n\nTo reserve a space in North County Mountain View Shelter, contact Jodi Harmon: 408-590-8196, JHarmon@homefirstscc.org.",
+                option: 'HomeFirst',
+                parent: obj._id,
+            });
+            homefirst.save(function(err, obj) {
+                if (err) throw err;
+            })
+        });
+        var employment = new SurveyQuestion({
+            question: "Got it - you're looking for employment-related information. Which of the following employment services are you interested in right now?",
+            option: 'Employment',
+            parent: obj._id,
+        });
+        employment.save(function(err, obj) {
+            if (err) throw err;
         });
         var food = new SurveyQuestion({
             question: "Got it - you're looking for food-related information. Which of the following food services are you interested in right now?",
